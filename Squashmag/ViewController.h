f30b778daa261eeb4b21794716397641c9c84024
AbbinVarghese <paadam2@gmail.com>
2016-05-31 11:50:35 +0530

    Initial Commit

---
+++ Squashmag/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Squashmag
+//
+//  Created by Abbin Varghese on 31/05/16.
+//  Copyright © 2016 Abbin Varghese. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
